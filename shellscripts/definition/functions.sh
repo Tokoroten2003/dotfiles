72afaa8410d08f55b86dbde4865b7be8bacac180
Chikurin1014 <chikurin1014@gmail.com>
2024-06-24 00:11:58 +0900

    fixed bug

--- shellscripts/definition/functions.sh
+++ shellscripts/definition/functions.sh
@@ -3,7 +3,6 @@
 function install_apt() {
     arg=$1
     echo "Installing $arg"
-    dpkg -l "$arg" > /dev/null 2>&1 && echo "  -> Already installed" && return 1 # package is already installed
-    sudo apt-get install -y "$arg" > /dev/null 2>&1 || echo "  -> Not available in apt" && return 2 # package cannot be installed by apt
-    return 0
+    sudo apt-get install -y "$arg" > /dev/null
+    return $?
 }
